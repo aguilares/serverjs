028afc52a41404ae9239de3a7a4ca19863ef41bf
aguilares <gustavoaguilarea@mgail.com>
2023-02-17 16:49:56 -0400

    arreglo

--- src/modelo/bdConfig.js
+++ src/modelo/bdConfig.js
@@ -2,10 +2,19 @@ import {createPool} from "mysql2/promise"
 import { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER } from '../config.js'
 
 const pool = createPool({
- host: DB_HOST,
- user: DB_USER,
- password: DB_PASSWORD,
- database:DB_DATABASE
+ host: 'containers-us-west-110.railway.app',
+ user: 'root',
+ password: 'ZHFSW66d5vLgbSo69iFg',
+ database:'railway'
 })
+
+// const pool = createPool({
+//     host: DB_HOST,
+//     user: DB_USER,
+//     password: DB_PASSWORD,
+//     database:DB_DATABASE
+//    })
+   
+
 
 export default pool

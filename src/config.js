81bb894c1c0688995c93ed77cbbf0d0884495e14
aguilares <gustavoaguilarea@mgail.com>
2023-02-17 16:43:50 -0400

    arreglo

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ config()
 
 export const PORT = process.env.PORT || 3001
 
-export const DB_HOST = process.env.DB_HOST || 'localhost';
-export const DB_USER = process.env.DB_USER || 'spclaver';
-export const DB_PASSWORD = process.env.DB_PASSWORD || '1234';
-export const DB_DATABASE = process.env.DB_DATABASE || 'sp';
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_HOST = process.env.DB_HOST || 'containers-us-west-110.railway.app';
+export const DB_USER = process.env.DB_USER || 'root';
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'ZHFSW66d5vLgbSo69iFg';
+export const DB_DATABASE = process.env.DB_DATABASE || 'railway';
+export const DB_PORT = process.env.DB_PORT || 6753;
 export const KEY = process.env.KEY || 'KEY2022'
 export const CLAVEGMAIL = process.env.CLAVEGMAIL || 'chwsjbjcespeucol'

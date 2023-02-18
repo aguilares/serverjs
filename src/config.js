334abd6be3e6f649dbfd4ace73ce3f803e89ed20
aguilares <gustavoaguilarea@mgail.com>
2023-02-18 18:08:47 -0400

    test

--- src/config.js
+++ src/config.js
@@ -4,9 +4,9 @@ config()
 export const PORT = process.env.PORT || 3001
 
 export const DB_HOST = process.env.DB_HOST || 'mysql-spclaver.alwaysdata.net';
-export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'ZHFSW66d5vLgbSo69iFg';
+export const DB_USER = process.env.DB_USER || 'spclaver_';
+export const DB_PASSWORD = process.env.DB_PASSWORD || '13616192Ch';
 export const DB_DATABASE = process.env.DB_DATABASE || 'spclaver_sp';
-export const DB_PORT = process.env.DB_PORT || 6753;
+export const DB_PORT = process.env.DB_PORT || 3306;
 export const KEY = process.env.KEY || 'KEY2022'
 export const CLAVEGMAIL = process.env.CLAVEGMAIL || 'chwsjbjcespeucol'
